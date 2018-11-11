51dc087d6b274774bf2bedf59c42dcfbeae275df
praveen <praveen.pulkanti@gmail.com>
2018-11-11 22:04:17 +0530

    updated with userpolicynumber validation

--- PolicyManagement/src/main/java/com/pms/controllers/UserPolicyValidation.java
+++ PolicyManagement/src/main/java/com/pms/controllers/UserPolicyValidation.java
@@ -2,25 +2,22 @@
 package com.pms.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pms.dao.PolicyDataRepo;
 import com.pms.service.Service1;
 @RestController
 @CrossOrigin
-@ComponentScans(value = { @ComponentScan("com.cts.product.dao"), @ComponentScan("com.cts.product.service") })
-@RequestMapping(value = "/products")
+
+
 
 public class UserPolicyValidation {
 	@Autowired
 	Service1 simple;
 	
-	
+	//@GetMapping(value = "/filter/{userid,policyId}")
 public boolean UserPolvalidation(@PathVariable("userid") String userid,@PathVariable("policyId") String policyId)
 {
 	
